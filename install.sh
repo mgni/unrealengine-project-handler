7aad3fb94ee3e857ea10f1a3a1c163c7f4c7c582
Mg Ni <soemintun007@gmail.com>
2024-05-06 10:10:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+#---user----
+#SHARE_DIR="$HOME/.local/share"
+#SHELL_DIR="$HOME/.local/bin"
+
+#----system----
 SHARE_DIR="/usr/share"
 SHELL_DIR="/usr/local/bin"
+
 RES_DIR="$PWD/res"
 ICON_DIR="$RES_DIR/icon"
 ENGINE="$PWD/Engine/Binaries/Linux/UnrealEditor"
 ENGINE_FLAGS="-nohighdpi"
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo 'Not running as root! Try run with sudo'
+    exit
+fi
 
 if [[ ! -d $RES_DIR ]]; then
     echo 'Error: res Folder Not Found!'
@@ -35,10 +46,10 @@ install -Dm755 "$RES_DIR/unrealengine.sh" "$SHELL_DIR/unrealengine"
 for x in 36 48 64 72 96 128 256 512
 do
     magick -density $x -background none "$ICON_DIR/unreal-engine.svg" "$ICON_DIR/unreal-engine-$x.png"
-    xdg-icon-resource install --novendor --noupdate --size $x "$ICON_DIR/unreal-engine-$x.png" unreal-engine
+    xdg-icon-resource install --novendor --noupdate --mode system --size $x "$ICON_DIR/unreal-engine-$x.png" unreal-engine
 
     magick -density $x -background none "$ICON_DIR/x-uproject.svg" "$ICON_DIR/project-$x.png"
-    xdg-icon-resource install --novendor --noupdate --context mimetypes --size $x "$ICON_DIR/project-$x.png" unrealengine-project
+    xdg-icon-resource install --novendor --noupdate --mode system --context mimetypes --size $x "$ICON_DIR/project-$x.png" unrealengine-project
 
     rm "$ICON_DIR/unreal-engine-$x.png"
     rm "$ICON_DIR/project-$x.png"
@@ -50,12 +61,3 @@ update-mime-database "$SHARE_DIR/mime"
 update-desktop-database "$SHARE_DIR/applications"
 
 echo 'Installation done!'
-
-#get engine version number
-# cat UnrealEditor.version | python -c "import sys, json; obj=json.load(sys.stdin); print(obj['MajorVersion'],obj['MinorVersion'],obj['PatchVersion'])"
-
-#check if file exist
-#[ -f $PWD/Engine/Binaries/Linux/UnrealEditor ] && echo "OKEY" 
-
-#replace text
-#engine=$PWD/Engine/; sed 's^Exe.*^Exec='$engine'^'  $PWD/res/unreal-engine.desktop
