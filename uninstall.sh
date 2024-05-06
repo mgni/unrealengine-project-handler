23d5fab820099dc3a6b68e8f5343dacd558133ca
Mg Ni <soemintun007@gmail.com>
2024-05-06 10:12:28 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+#---user----
+#SHARE_DIR="$HOME/.local/share"
+#SHELL_DIR="$HOME/.local/bin"
+
+#----system----
 SHARE_DIR="/usr/share"
 SHELL_DIR="/usr/local/bin"
+
 RES_DIR="$PWD/res"
 ICON_DIR="$RES_DIR/icon"
 ENGINE="$PWD/Engine/Binaries/Linux/UnrealEditor"
 ENGINE_FLAGS="-nohighdpi"
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo 'Not running as root! Try run with sudo'
+    exit
+fi
 
 echo -e "\nProceeding ..... \n"
 
@@ -15,8 +26,8 @@ rm "$SHELL_DIR/unrealengine"
 
 for x in 36 48 64 72 96 128 256 512
 do
-    xdg-icon-resource uninstall --size $x unreal-engine
-    xdg-icon-resource uninstall --context mimetypes --size $x unrealengine-project
+    xdg-icon-resource uninstall --novendor --mode system --size $x unreal-engine
+    xdg-icon-resource uninstall --novendor --mode system --context mimetypes --size $x unrealengine-project
 done
 
 gtk-update-icon-cache "$SHARE_DIR/icons/hicolor"
